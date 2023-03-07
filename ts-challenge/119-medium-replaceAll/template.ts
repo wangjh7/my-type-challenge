17d8ebbfcbbe0ce44bb6a931a649667b83d29a55
johann <584971501@qq.com>
2023-03-07 15:42:48 +0800

    replace all

---
+++ ts-challenge/119-medium-replaceAll/template.ts
@@ -0,0 +1,2 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends "" ? S : S extends `${infer Left}${From}${infer Right}` ? `${Left}${To}${ReplaceAll<Right,From,To>}` : S
+
